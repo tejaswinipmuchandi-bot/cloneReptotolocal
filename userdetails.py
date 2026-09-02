762efecc1878e8107fcb8a97532848ad2938585d
tejaswini <tejaswinipmuchandi@gmail.com>
2026-09-02 14:54:48 +0530

    saving the modified chnages

--- userdetails.py
+++ userdetails.py
@@ -1,4 +1,6 @@
 firstname = input("Enter your first name")
 last name = input("Enter ypur last name")
+city = input("Enter your city")
 
 print("User first name and last name ", firstname + " "+lastname)
+print("User city", city)
